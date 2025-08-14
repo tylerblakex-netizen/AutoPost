f8fc5b9d80dbdee34b77436f186d023fcb0fad18
tylerblakex-netizen <tylerblakex@gmail.com>
2025-08-14 21:05:56 +0100

    Fix filename length limit

--- src/main/java/com/autopost/FilenameUtil.java
+++ src/main/java/com/autopost/FilenameUtil.java
@@ -5,7 +5,9 @@ public class FilenameUtil {
   public static String sanitizeBase(String s){
     String n=Normalizer.normalize(s, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]","");
     n=n.replaceAll("[^A-Za-z0-9._-]+","_").replaceAll("_+","_").replaceAll("(^_|_$)","");
-    if(n.length()>60) n=n.substring(0,60); if(!n.toLowerCase().endsWith(".mp4")) n+=".mp4"; return n;
+    if(!n.toLowerCase().endsWith(".mp4")) n+=".mp4";
+    if(n.length()>60) n=n.substring(0,56)+".mp4";
+    return n;
   }
   public static String buildName(String collab,String type,int i){
     String date=LocalDate.now().format(D); String base=(collab==null||collab.isBlank()? "clip": collab)+"_"+type+"_"+String.format("%02d",i);
