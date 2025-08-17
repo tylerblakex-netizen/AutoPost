cc93a2d48ce49bd79fb4e7bd935663604506bace
tylerblakex-netizen <tylerblakex@gmail.com>
2025-08-17 10:47:56 +0100

    build: normalize settings with pluginManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
 rootProject.name = "AutoPost"
